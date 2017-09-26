f79a76aa26630826282d70e512b929b41ec82aa0
Ryohei Sogo <ryohei.sogo+github@gmail.com>
2017-09-26 16:30:52 +0900

    アプリの設定ファイルを追加

---
+++ app/src/main/java/jp/apppot/android/sample/AppPotConfig.java
@@ -0,0 +1,13 @@
+package jp.apppot.android.sample;
+
+public class AppPotConfig {
+    static public int companyId = 99;
+    static public String appVersion = "1.0";
+    static public String appKey = "dummy key";
+    static public String appID = "jp.apppot.android.sample.SimpleAndroidSampleApp";
+    static public String hostName = "dummy hostname";
+    static public String contextRoot = "dummy root";
+    static public int portNumber = 80;
+    static public boolean isUseSSL = false;
+    static public boolean isUsePushNotification = false;
+}
